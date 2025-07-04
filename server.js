0f927acc084977cf7ac15aa0d82df9f7cdc28cb6
Lutfiyadi Afandi <lutfiyadiafandi@gmail.com>
2025-07-04 14:31:29 +0800

    fix: bind server to all network interfaces

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
