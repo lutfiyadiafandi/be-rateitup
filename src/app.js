df745f0b4511fc3d61e76ed6c6e3cdd76de25229
Lutfiyadi Afandi <lutfiyadiafandi@gmail.com>
2025-07-04 11:44:14 +0800

    fix: testing deploy without app

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const authRoutes = require("./routes/auth.routes");
-const userRoutes = require("./routes/user.routes");
-const restaurantRoutes = require("./routes/restaurant.routes");
-const reviewRoutes = require("./routes/review.routes");
-const commentRoutes = require("./routes/comment.routes");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const authRoutes = require("./routes/auth.routes");
+// const userRoutes = require("./routes/user.routes");
+// const restaurantRoutes = require("./routes/restaurant.routes");
+// const reviewRoutes = require("./routes/review.routes");
+// const commentRoutes = require("./routes/comment.routes");
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", restaurantRoutes);
-app.use("/api", reviewRoutes);
-app.use("/api", commentRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api", userRoutes);
+// app.use("/api", restaurantRoutes);
+// app.use("/api", reviewRoutes);
+// app.use("/api", commentRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
